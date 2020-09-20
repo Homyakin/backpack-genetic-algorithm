e254fe69085e356a16b98c1a0ddd59e700b9676d
gultiaeva <angultiaev@gmail.com>
2020-09-20 22:46:41 +0300

    improved algorithm

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
-from models import Backpack, Item
-from genetic_functions import create_start_generation, create_new_generation
 
-print("Задайте количество различных типов предметов")
-types_count = int(input())
+from models import BackpackFactory, Item
+
+
+types_count = int(input("Задайте количество различных типов предметов: "))
 
 print("Выберите способ задания начальных условий:")
 print("1. Ручной")
@@ -11,26 +11,45 @@ print("2. Случайный")
 
 case = input()
 
-backpack = Backpack(types_count)
+if case == "1":
+    max_volume = int(input("Введите объем рюкзака: "))
+    alpha = int(input("Введите количество лучших особей из предыдущего поколения, которые будут в следующем: "))
+    max_generations = int(input("Введите максимальное количество поколений: "))
+    max_specimen = int(input("Введите максимальное количество особей в поколении: "))
+    crossover_type = input("Введите тип кроссовера (random, avg): ")
+    crossover_probability = float(input("Введите вероятность кроссовера: "))
+    mutation_probability = float(input("Введите вероятность мутации: "))
+    epsilon = float(input("Введите точность функции приспособленности: "))
 
-if case == "2":
-    items = []
-    for i in range(types_count):
-        item = Item(i, random.randint(1, 20), random.randint(1, 20))
-        items.append(item)
-    backpack.items = items
-    print("Список предметов:", backpack.items)
-    print("Максимальная вместимость:", backpack.max_volume)
-    print("Количество лучших особей из предыдущего поколения:", backpack.alpha)
-    print("Точность функции приспособленности:", backpack.epsilon)
-    print("Максимальное количество поколений:", backpack.max_generations)
-    print("Максимальное количество особей в поколении:", backpack.max_individuals)
-    print("Вероятность кроссовера: ", backpack.crossover_probability, "%", sep='')
-    print("Вероятность мутации: ", backpack.mutation_probability, "%", sep='')
+    items = [Item(i, random.randint(1, 20), random.randint(1, 20)) for i in range(types_count)]
+    backpack = BackpackFactory(items, 
+                               max_volume, 
+                               alpha, 
+                               max_generations, 
+                               max_specimen, 
+                               crossover_type, 
+                               crossover_probability, 
+                               mutation_probability, 
+                               epsilon)
 
-generation = create_start_generation(backpack)
-print(generation[0])
+elif case == "2":
+    items = [Item(i, random.randint(1, 20), random.randint(1, 20)) for i in range(types_count)]
+    backpack = BackpackFactory(items)
 
-for _ in range(backpack.max_generations):
-    generation = create_new_generation(backpack, generation)
-    print(generation[0])
+else:
+    exit(1)
+
+print("Список предметов: ", end='')
+for item in backpack.items:
+    print(item)
+print()
+
+print("Максимальная вместимость: ", backpack.max_volume)
+print("Количество лучших особей из предыдущего поколения: ", backpack.alpha)
+print("Точность функции приспособленности: ", backpack.epsilon)
+print("Максимальное количество поколений: ", backpack.max_generations)
+print("Максимальное количество особей в поколении: ", backpack.max_specimen)
+print("Вероятность кроссовера: ", backpack.crossover_probability, sep='')
+print("Вероятность мутации: ", backpack.mutation_probability, sep='')
+
+backpack.evolve()
--- models.py
+++ models.py
@@ -1,29 +1,211 @@
 import random
 
 
-class Backpack:
-    def __init__(self, types_count):
-        self.types_count = types_count
-        self.items = []  # предметы (Item)
-        self.max_volume = random.randint(50, 300)  # максимальный объем рюкзака
-        self.alpha = 2  # количество лучших особей из предыдущего поколения
-        self.epsilon = 0.05  # точность функции приспособленности
-        self.max_generations = random.randint(1000, 3000)  # максимальное количество поколений
-        self.max_individuals = random.randint(500, 2000)  # максимальное количество особей в поколении
-        self.crossover_probability = random.randint(85, 99)  # вероятность кроссовера
-        self.mutation_probability = random.randint(3, 6)  # вероятность мутации
+class BackpackFactory:
+    """
+    Завод по укладыванию вещей в рюкзаки.
+
+    :param items: Предметы
+    :param max_volume: Максимальный объем рюкзака
+    :param alpha: Количество лучших особей из предыдущего поколения,
+        которые пойдут в следующее
+    :param max_generations: Максимальное количество поколений
+    :param max_specimen: Максимальное количество особей в поколении
+    :param crossover_type: Тип кроссовера. 
+        Random -- случайный выбор 
+        Avg -- среднее между родителями
+    :param crossover_probability: Вероятность кроссовера
+    :param mutation_probability: Вероятность мутации
+    :param epsilon: Точность функции приспособленности
+    """
+    def __init__(self,  
+                 items,
+                 max_volume=50, 
+                 alpha=2, 
+                 max_generations=5000, 
+                 max_specimen=100, 
+                 crossover_type='random',
+                 crossover_probability=.85, 
+                 mutation_probability=.5,
+                 epsilon=.01):
+
+        assert crossover_type in ('random', 'avg'), 'Invalid crossover type'
+
+        self.items = items  # List of Item
+        self.types_count = len(items)
+        self.max_volume = max_volume
+        self.alpha = alpha
+        self.max_generations = max_generations
+        self.max_specimen = max_specimen
+        if crossover_type == 'rand':
+            self.crossover = self.rand_crossover
+        else:
+            self.crossover = self.avg_crossover
+        self.crossover_probability = crossover_probability
+        self.mutation_probability = mutation_probability
+        self.epsilon = epsilon
+
+    def create_rand_backpack(self):
+        """Создает случайную допустимую особь."""
+
+        backpack_volume = 0
+        available_items = self.items
+        item_counts = [0] * self.types_count  # изначально особь пустая
+        while len(available_items) != 0:
+            available_items = list(filter(lambda x: x.volume <= self.max_volume - backpack_volume,
+                                          available_items))
+            if len(available_items) == 0:
+                break
+            item = random.choice(available_items)  # выбираем случайный предмет
+
+            # выбираем случайное количество предмета
+            if len(available_items) == 1:
+                item_count = int((self.max_volume - backpack_volume) / item.volume)
+            else:
+                item_count = random.randint(1, (self.max_volume - backpack_volume) // item.volume)
+
+            item_counts[item.number] += item_count  # добавляем количества предмета на соответствующую позицию
+            backpack_volume += item_count * item.volume  # увеличиваем текущую вместимость особи
+
+        return Backpack(self.items, item_counts)
+
+    def create_start_generation(self):
+        """Создает стартовое поколение."""
+
+        backpacks = [self.create_rand_backpack() for _ in range(self.max_specimen)]
+        return Generation(backpacks)
+
+    def rand_crossover(self, parent_1, parent_2):
+        """Проводит случайный кроссовер (случайный выбор частей родителей)."""
+
+        for _ in range(10):  # TODO: чекнуть кол-во попыток
+
+            crossover_counts = [random.choice([par1_arg, par2_arg]) 
+                                for par1_arg, par2_arg in zip(parent_1.item_counts, 
+                                                              parent_2.item_counts)]
+            backpack = Backpack(self.items, crossover_counts)
+            if backpack.volume <= self.max_volume:
+                return backpack
+
+        return parent_1 if parent_1.cost > parent_2.cost else parent_2
+
+    def avg_crossover(self, parent_1, parent_2):
+        """Проводит avg кроссовер (Среднее частей родителей)."""
+        for _ in range(10):  # TODO: чекнуть кол-во попыток
+            crossover_counts = [(par1_arg + par2_arg) // 2 
+                                for par1_arg, par2_arg in zip(parent_1.item_counts,
+                                                              parent_2.item_counts)]
+            backpack = Backpack(self.items, crossover_counts)
+            if backpack.volume <= self.max_volume:
+                return backpack
+            print('here')
+        return parent_1 if parent_1.cost > parent_2.cost else parent_2
+
+    def create_new_generation(self, generation):
+        """Создает новое поколение особей."""
+        new_backpacks = []
+
+        for _ in range(2 * self.max_specimen): 
+            if random.random() <= self.mutation_probability:
+                mutated_backpack = self.create_rand_backpack()
+                new_backpacks.append(mutated_backpack)
+                continue
+
+            parent_1, parent_2 = random.sample(list(generation), k=2)
+
+            if random.random() <= self.crossover_probability:
+                crossovered_backpack = self.crossover(parent_1, parent_2)
+                new_backpacks.append(crossovered_backpack)
+                continue
+
+            new_backpacks.append(parent_1 if parent_1.cost > parent_2.cost else parent_2)
+
+        alpha_best = sorted(generation, key=lambda x: x.cost, reverse=True)[:self.alpha]
+        new_backpacks.extend(alpha_best)
+        new_backpacks = sorted(new_backpacks, key=lambda x: x.cost, reverse=True)
+
+        return Generation(new_backpacks[0:self.max_specimen])
+
+    def evolve(self):
+        """Запускает процесс эволюции."""
+
+        generation = self.create_start_generation()
+        print('Поколение 0')
+        print(generation)
+        max_cost = generation.cost
+
+        for i in range(1, self.max_generations+1):
+            new_generation = self.create_new_generation(generation)
+            if i % 10 == 0:
+                print(f"Приспособленность поколения {i}: {generation.cost:.4f}")
+            if abs(new_generation.cost - max_cost) < self.epsilon:
+                print(f"Поколение {i} -- выход")
+                break
+
+            generation = new_generation
+        print()
+
+        print(f"Поколение {i}: ")
+        print(generation)
 
 
-class Individual:
-    def __init__(self, items: list, cost: int):
-        self.items = items
-        self.cost = cost
+class Generation:
+    """
+    Поколение особей.
+
+    :param backpacks: Список особей
+    """
+    def __init__(self, backpacks):
+        self.backpacks = backpacks
+
+    @property
+    def cost(self):
+        return sum(item.cost for item in self) / len(self)
+
+    def __len__(self):
+        return len(self.backpacks)
+
+    def __iter__(self):
+        return iter(self.backpacks)
 
     def __repr__(self):
-        return "[Стоимость {}; веса: {}]".format(self.cost, self.items)
+        objs = '\n'.join(backpack.__repr__() for backpack in self)
+        cost = self.cost
+        return f'''Объекты: {objs}\nПриспособленность поколения: {cost}'''
+
+    
+class Backpack:
+    """
+    Рюкзак.
+
+    :param items: Список всех вещей
+    :param item_counts: Список из количеств каждой вещи, лежащих в рюкзаке 
+    """
+    
+    def __init__(self, items, item_counts):
+        self.items = items
+        self.item_counts = item_counts
+
+    @property
+    def cost(self):
+        return sum([cnt * item.cost for cnt, item in zip(self.item_counts, self.items)])
+
+    @property
+    def volume(self):
+        return sum([cnt * item.volume for cnt, item in zip(self.item_counts, self.items)])
+
+    def __repr__(self):
+        return "[Стоимость {}; Предметы: {}]".format(self.cost, self.item_counts)
 
 
 class Item:
+    """
+    Вещь.
+    
+    :param number: Порядковый номер вещи
+    :param volume: Объем вещи
+    :param cost: Стоимость вещи
+    """
     def __init__(self, number: int, volume: int, cost: int):
         self.number = number
         self.volume = volume
